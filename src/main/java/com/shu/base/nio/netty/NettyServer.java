20efc792813f59c81cb7f3988d398ab3e3256ca7
shu616048151 <616048151@qq.com>
2019-08-08 16:20:58 +0800

    netty的客户端和服务端代码实现

---
+++ src/main/java/com/shu/base/nio/netty/NettyServer.java
@@ -0,0 +1,39 @@
+package com.shu.base.nio.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * @author shuxibing
+ * @date 2019/8/8 6:53
+ * @uint d9lab
+ * @Description:
+ */
+public class NettyServer {
+    public static void main(String[] args){
+        ServerBootstrap bootstrap=new ServerBootstrap();
+        NioEventLoopGroup boss=new NioEventLoopGroup();
+        NioEventLoopGroup worker=new NioEventLoopGroup();
+
+        bootstrap.group(boss,worker).channel(NioServerSocketChannel.class).childHandler(new ChannelInitializer<NioSocketChannel>() {
+
+            @Override
+            protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                nioSocketChannel.pipeline().addLast(new StringDecoder());
+                nioSocketChannel.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
+                    @Override
+                    protected void channelRead0(ChannelHandlerContext channelHandlerContext, String msg) throws Exception {
+                        System.out.println(msg);
+                    }
+                });
+            }
+        }).bind(8000);
+
+    }
+}
