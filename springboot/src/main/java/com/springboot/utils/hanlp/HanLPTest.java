cf4149166466a12957ace72565a4c8c3bf1d7f1f
shu616048151 <616048151@qq.com>
2019-11-06 19:41:07 +0800

    hanlp入门测试

---
+++ springboot/src/main/java/com/springboot/utils/hanlp/HanLPTest.java
@@ -0,0 +1,283 @@
+package com.springboot.utils.hanlp;
+
+import com.hankcs.hanlp.HanLP;
+import com.hankcs.hanlp.collection.AhoCorasick.AhoCorasickDoubleArrayTrie;
+import com.hankcs.hanlp.dictionary.CoreDictionary;
+import com.hankcs.hanlp.dictionary.CustomDictionary;
+import com.hankcs.hanlp.dictionary.py.Pinyin;
+import com.hankcs.hanlp.model.crf.CRFLexicalAnalyzer;
+import com.hankcs.hanlp.seg.Dijkstra.DijkstraSegment;
+import com.hankcs.hanlp.seg.NShort.NShortSegment;
+import com.hankcs.hanlp.seg.Segment;
+import com.hankcs.hanlp.seg.common.Term;
+import com.hankcs.hanlp.tokenizer.IndexTokenizer;
+import com.hankcs.hanlp.tokenizer.NLPTokenizer;
+import com.hankcs.hanlp.tokenizer.SpeedTokenizer;
+import com.hankcs.hanlp.tokenizer.StandardTokenizer;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author shuxibing
+ * @date 2019/11/6 18:46
+ * @uint d9lab
+ * @Description:  自然语言处理工具测试
+ */
+public class HanLPTest {
+
+
+    @Test
+    public void test(){
+        System.out.println(HanLP.segment("你好，欢迎使用HanLP汉语处理包！"));
+    }
+
+    @Test
+    public void test2(){
+        List<Term> termList = StandardTokenizer.segment("我是舒志阳");
+        System.out.println(termList);
+
+
+    }
+
+
+    @Test
+    public void test3(){
+        List<Term> termList = IndexTokenizer.segment("主副食品");
+        for (Term term : termList)
+        {
+            System.out.println(term + " [" + term.offset + ":" + (term.offset + term.word.length()) + "]");
+        }
+    }
+
+    @Test
+    public void test4(){
+        Segment nShortSegment = new NShortSegment().enableCustomDictionary(false).enablePlaceRecognize(true).enableOrganizationRecognize(true);
+        Segment shortestSegment = new DijkstraSegment().enableCustomDictionary(false).enablePlaceRecognize(true).enableOrganizationRecognize(true);
+        String[] testCase = new String[]{
+                "今天，刘志军案的关键人物,山西女商人丁书苗在市二中院出庭受审。",
+                "刘喜杰石国祥会见吴亚琴先进事迹报告团成员",
+        };
+        for (String sentence : testCase)
+        {
+            System.out.println("N-最短分词：" + nShortSegment.seg(sentence) + "\n最短路分词：" + shortestSegment.seg(sentence));
+        }
+    }
+
+    @Test
+    public void test5() throws IOException {
+        CRFLexicalAnalyzer analyzer = new CRFLexicalAnalyzer();
+        String[] tests = new String[]{
+                "商品和服务",
+                "上海华安工业（集团）公司董事长谭旭光和秘书胡花蕊来到美国纽约现代艺术博物馆参观",
+                "微软公司於1975年由比爾·蓋茲和保羅·艾倫創立，18年啟動以智慧雲端、前端為導向的大改組。" // 支持繁体中文
+        };
+        for (String sentence : tests)
+        {
+            System.out.println(analyzer.analyze(sentence));
+        }
+    }
+
+
+    @Test
+    public void test6(){
+        String[] testCase = new String[]{
+                "一桶冰水当头倒下，微软的比尔盖茨、Facebook的扎克伯格跟桑德博格、亚马逊的贝索斯、苹果的库克全都不惜湿身入镜，这些硅谷的科技人，飞蛾扑火似地牺牲演出，其实全为了慈善。",
+                "世界上最长的姓名是简森·乔伊·亚历山大·比基·卡利斯勒·达夫·埃利奥特·福克斯·伊维鲁莫·马尔尼·梅尔斯·帕特森·汤普森·华莱士·普雷斯顿。",
+        };
+        Segment segment = HanLP.newSegment().enableTranslatedNameRecognize(true);
+        for (String sentence : testCase)
+        {
+            List<Term> termList = segment.seg(sentence);
+            System.out.println(termList);
+        }
+    }
+
+    @Test
+    public void test7(){
+        String text = "江西鄱阳湖干枯，中国最大淡水湖变成大草原";
+        System.out.println(SpeedTokenizer.segment(text));
+        long start = System.currentTimeMillis();
+        int pressure = 1000000;
+        for (int i = 0; i < pressure; ++i)
+        {
+            SpeedTokenizer.segment(text);
+        }
+        double costTime = (System.currentTimeMillis() - start) / (double)1000;
+        System.out.printf("分词速度：%.2f字每秒", text.length() * pressure / costTime);
+    }
+
+    @Test
+    public void  test8(){
+        // 动态增加
+        CustomDictionary.add("攻城狮");
+        // 强行插入
+        CustomDictionary.insert("白富美", "nz 1024");
+        // 删除词语（注释掉试试）
+//        CustomDictionary.remove("攻城狮");
+        System.out.println(CustomDictionary.add("单身狗", "nz 1024 n 1"));
+        System.out.println(CustomDictionary.get("单身狗"));
+
+        String text = "攻城狮逆袭单身狗，迎娶白富美，走上人生巅峰";  // 怎么可能噗哈哈！
+
+        // AhoCorasickDoubleArrayTrie自动机扫描文本中出现的自定义词语
+        final char[] charArray = text.toCharArray();
+        CustomDictionary.parseText(charArray, new AhoCorasickDoubleArrayTrie.IHit<CoreDictionary.Attribute>()
+        {
+            @Override
+            public void hit(int begin, int end, CoreDictionary.Attribute value)
+            {
+                System.out.printf("[%d:%d]=%s %s\n", begin, end, new String(charArray, begin, end - begin), value);
+            }
+        });
+
+        // 自定义词典在所有分词器中都有效
+        System.out.println(HanLP.segment(text));
+    }
+
+    /**
+     * 人名识别
+     */
+    @Test
+    public void test9(){
+        String[] testCase = new String[]{
+                "签约仪式前，秦光荣、李纪恒、仇和等一同会见了参加签约的企业家。",
+                "王国强、高峰、汪洋、张朝阳光着头、韩寒、小四",
+                "张浩和胡健康复员回家了",
+                "王总和小丽结婚了",
+                "编剧邵钧林和稽道青说",
+                "这里有关天培的有关事迹",
+                "龚学平等领导,邓颖超生前",
+        };
+        Segment segment = HanLP.newSegment().enableNameRecognize(true);
+        for (String sentence : testCase)
+        {
+            List<Term> termList = segment.seg(sentence);
+            System.out.println(termList);
+        }
+    }
+
+    /**
+     * 地名识别
+     */
+    @Test
+    public void test10(){
+        String[] testCase = new String[]{
+                "武胜县新学乡政府大楼门前锣鼓喧天",
+                "蓝翔给宁夏固原市彭阳县红河镇黑牛沟村捐赠了挖掘机",
+        };
+        Segment segment = HanLP.newSegment().enablePlaceRecognize(true);
+        for (String sentence : testCase)
+        {
+            List<Term> termList = segment.seg(sentence);
+            System.out.println(termList);
+        }
+    }
+
+    /**
+     * 机构名识别
+     */
+    @Test
+    public void test11(){
+        String[] testCase = new String[]{
+                "我在上海林原科技有限公司兼职工作，",
+                "我经常在武汉理工大学图书馆学习，",
+                "偶尔去地中海影城看电影。",
+        };
+        Segment segment = HanLP.newSegment().enableOrganizationRecognize(true);
+        for (String sentence : testCase)
+        {
+            List<Term> termList = segment.seg(sentence);
+            System.out.println(termList);
+        }
+    }
+    @Test
+    public void test12(){
+        String content = "程序员(英文Programmer)是从事程序开发、维护的专业人员。一般将程序员分为程序设计人员和程序编码人员，但两者的界限并不非常清楚，特别是在中国。软件从业人员分为初级程序员、高级程序员、系统分析员和项目经理四大类。";
+        List<String> keywordList = HanLP.extractKeyword(content, 5);
+        System.out.println(keywordList);
+    }
+
+    @Test
+    public void test13(){
+        String document = "算法可大致分为基本算法、数据结构的算法、数论算法、计算几何的算法、图的算法、动态规划以及数值分析、加密算法、排序算法、检索算法、随机化算法、并行算法、厄米变形模型、随机森林算法。\n" +
+                "算法可以宽泛的分为三类，\n" +
+                "一，有限的确定性算法，这类算法在有限的一段时间内终止。他们可能要花很长时间来执行指定的任务，但仍将在一定的时间内终止。这类算法得出的结果常取决于输入值。\n" +
+                "二，有限的非确定算法，这类算法在有限的时间内终止。然而，对于一个（或一些）给定的数值，算法的结果并不是唯一的或确定的。\n" +
+                "三，无限的算法，是那些由于没有定义终止定义条件，或定义的条件无法由输入的数据满足而不终止运行的算法。通常，无限算法的产生是由于未能确定的定义终止条件。";
+        List<String> sentenceList = HanLP.extractSummary(document, 3);
+        System.out.println(sentenceList);
+    }
+
+    @Test
+    public void test14(){
+        String text = "重载不是重任";
+        List<Pinyin> pinyinList = HanLP.convertToPinyinList(text);
+        System.out.print("原文,");
+        for (char c : text.toCharArray())
+        {
+            System.out.printf("%c,", c);
+        }
+        System.out.println();
+
+        System.out.print("拼音（数字音调）,");
+        for (Pinyin pinyin : pinyinList)
+        {
+            System.out.printf("%s,", pinyin);
+        }
+        System.out.println();
+
+        System.out.print("拼音（符号音调）,");
+        for (Pinyin pinyin : pinyinList)
+        {
+            System.out.printf("%s,", pinyin.getPinyinWithToneMark());
+        }
+        System.out.println();
+
+        System.out.print("拼音（无音调）,");
+        for (Pinyin pinyin : pinyinList)
+        {
+            System.out.printf("%s,", pinyin.getPinyinWithoutTone());
+        }
+        System.out.println();
+
+        System.out.print("声调,");
+        for (Pinyin pinyin : pinyinList)
+        {
+            System.out.printf("%s,", pinyin.getTone());
+        }
+        System.out.println();
+
+        System.out.print("声母,");
+        for (Pinyin pinyin : pinyinList)
+        {
+            System.out.printf("%s,", pinyin.getShengmu());
+        }
+        System.out.println();
+
+        System.out.print("韵母,");
+        for (Pinyin pinyin : pinyinList)
+        {
+            System.out.printf("%s,", pinyin.getYunmu());
+        }
+        System.out.println();
+
+        System.out.print("输入法头,");
+        for (Pinyin pinyin : pinyinList)
+        {
+            System.out.printf("%s,", pinyin.getHead());
+        }
+        System.out.println();
+    }
+
+    @Test
+    public void test15(){
+        System.out.println(HanLP.convertToTraditionalChinese("用笔记本电脑写程序"));
+        System.out.println(HanLP.convertToSimplifiedChinese("「以後等妳當上皇后，就能買士多啤梨慶祝了」"));
+    }
+
+
+
+
+}
