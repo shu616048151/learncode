3a04d2428c04aaf4824e9d56b9d2da56061f88ad
shu616048151 <616048151@qq.com>
2019-12-28 15:35:46 +0800

    springboot的jpa入门

---
+++ springboot/src/main/java/com/springboot/jpa/base/JpaConfig.java
@@ -0,0 +1,15 @@
+package com.springboot.jpa.base;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * @author shuxibing
+ * @date 2019/12/28 15:28
+ * @uint d9lab
+ * @Description: jpa的一些配置信息
+ */
+@Configuration
+@EnableJpaRepositories(basePackages = "com.springboot.jpa.repostiory") //扫包和注解方式都可以实现
+public class JpaConfig {
+}
