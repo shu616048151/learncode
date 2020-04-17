bc80b878a5c3b2362704e56c14b14e5dc31feb86
码小研 <38661582+shu616048151@users.noreply.github.com>
2020-04-17 10:32:42 +0800

    更新 并发工具类

--- src/main/java/com/shu/base/concurrent/CountDownLatchTest.java
+++ src/main/java/com/shu/base/concurrent/CountDownLatchTest.java
@@ -10,6 +10,7 @@ import java.util.concurrent.Executors;
  * @uint d9lab
  * @Description:  在一些应用场合中，需要等待某个条件达到要求后才能做后面的事情；同时当线程都完成后也会触发事件，以便进行后面的操作。 这个时候就可以使用CountDownLatch
  * 。CountDownLatch最重要的方法是countDown()和await()，前者主要是倒数一次，后者是等待倒数到0，如果没有到达0，就只有阻塞等待了。
+ * 一般是主线程控制开关
  */
 // 一个CountDouwnLatch实例是不能重复使用的，也就是说它是一次性的，锁一经被打开就不能再关闭使用了，如果想重复使用，请考虑使用CyclicBarrier。
 public class CountDownLatchTest {
@@ -26,7 +27,7 @@ public class CountDownLatchTest {
             Runnable run = new Runnable() {
                 public void run() {
                     try {
-                        // 如果当前计数为零，则此方法立即返回。
+                        // 如果当前计数为零,则此方法立即返回。
                         // 一直处于等转态
                         begin.await();
                         Thread.sleep((long) (Math.random() * 10000));
@@ -40,8 +41,8 @@ public class CountDownLatchTest {
             };
             exec.submit(run);
         }
-        System.out.println("Game Start");
         // begin减一，开始游戏
+        System.out.println("Game Start");
         begin.countDown();
         // 等待end变为0，即所有选手到达终点，也就是等到所有选手的比赛状态
         end.await();
--- src/main/java/com/shu/base/concurrent/CyclicBarrierTest.java
+++ src/main/java/com/shu/base/concurrent/CyclicBarrierTest.java
@@ -11,7 +11,7 @@ import java.util.concurrent.Executors;
  * @author shuxibing
  * @date 2019/9/30 9:51
  * @uint d9lab
- * @Description:  CyclicBarrier同步线程辅助类,发生某种情况才会统一执行
+ * @Description:  CyclicBarrier同步线程辅助类,发生某种情况才会统一执行，自动触发，一般用于其他线程
  */
 public class CyclicBarrierTest {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -22,28 +22,29 @@ public class CyclicBarrierTest {
         executor.submit(new Thread(new Runner(barrier, "3号选手")));
         executor.shutdown();
     }
+
 }
 class Runner implements Runnable {
-    // 一个同步辅助类，它允许一组线程互相等待，直到到达某个公共屏障点 (common barrier point)
-    private CyclicBarrier barrier;
-    private String name;
-    public Runner(CyclicBarrier barrier, String name) {
-        super();
-        this.barrier = barrier;
-        this.name = name;
-    }
-    @Override
-    public void run() {
-        try {
-            Thread.sleep(1000 * (new Random()).nextInt(8));
-            System.out.println(name + " 准备好了...");
-            // barrier的await方法，在所有参与者都已经在此 barrier 上调用 await 方法之前，将一直等待。等所有线程到达的时候才开始执行下面的操作
-            barrier.await();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (BrokenBarrierException e) {
-            e.printStackTrace();
+        // 一个同步辅助类，它允许一组线程互相等待，直到到达某个公共屏障点 (common barrier point)
+        private CyclicBarrier barrier;
+        private String name;
+        public Runner(CyclicBarrier barrier, String name) {
+            super();
+            this.barrier = barrier;
+            this.name = name;
         }
-        System.out.println(name + " 起跑！");
-    }
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(1000 * (new Random()).nextInt(8));
+                System.out.println(name + " 准备好了...");
+                // barrier的await方法，在所有参与者都已经在此 barrier 上调用 await 方法之前，将一直等待。等所有线程到达的时候才开始执行下面的操作
+                barrier.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+            System.out.println(name + " 起跑！");
+        }
 }
