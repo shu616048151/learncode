e03189d0a47226ab00b8e0f31822657728f588f1
shu616048151 <616048151@qq.com>
2020-09-07 10:58:24 +0800

    背包问题

--- springboot/src/main/java/com/shu/alogrithm/DynamicProgramming.java
+++ springboot/src/main/java/com/shu/alogrithm/DynamicProgramming.java
@@ -23,9 +23,12 @@ package com.shu.alogrithm;
  */
 public class DynamicProgramming {
     public static void main(String[] args){
-        int[] arr={1,2,-1,-3,5,6,-2};
-        int length = LongestSubString(arr);
-        System.out.println(length);
+       int[] w={3,5,2,6,4};
+       int[] v={4,4,3,5,3};
+       int[] num={1,1,1,1,1};
+        System.out.println(package01(w,v,w.length,4));
+        System.out.println(packageFull(w,v,w.length,4));
+        System.out.println(packageMany(w,v,w.length,4,num));
     }
 
 
@@ -78,6 +81,116 @@ public class DynamicProgramming {
         }
         return Memo[n];
     }
+
+
+    /**
+     * 01背包问题
+     * @param w
+     * @param v
+     * @return
+     */
+   public static int package01(int[] w,int[] v,int n,int m){
+       //表示前i(可以表示为物品的标号，数量可以不为1)种物品恰好放入重量为m的背包时的最大价值
+       int[][] dp=new int[n+1][m+1];
+
+       //初始化数组
+       for(int i=0;i<n+1;i++){
+           dp[i][0] = 0;
+       }
+       for(int j=0;j<m+1;j++){
+           dp[0][j] = 0;
+       }
+
+        //物品的编号
+       for(int i=1;i<n+1;i++){
+           //背包的最大容量
+           for (int j=1;j<m+1;j++){
+               //物品的重量小于背包的容量
+               if (w[i-1] <= j){
+                   //加入物品 和没有放入背包之前做比较
+                   dp[i][j]=Math.max(dp[i-1][j],dp[i-1][j-w[i-1]]+v[i-1]);
+               } else{
+                   //该物品i不能放入，只能保持原来的物品数量的价值
+                   dp[i][j]=dp[i-1][j];
+               }
+           }
+       }
+     return dp[n][m];
+   }
+
+   /**
+    * f[i][v]=max{f[i-1][v-k*c[i]]+k*w[i]|0<=k*c[i]<=v} 物品可以加多次
+     * 完全背包问题(每个物品不限量)
+     * @param w
+     * @param v
+     * @return
+     */
+   public static int packageFull(int[] w,int[] v,int n,int m){
+       //表示前i件物品恰好放入重量为m的背包时的最大价值
+       int[][] dp=new int[n+1][m+1];
+
+       //初始化数组
+       for(int i=0;i<n+1;i++){
+           dp[i][0] = 0;
+       }
+       for(int j=0;j<m+1;j++){
+           dp[0][j] = 0;
+       }
+
+        //物品的编号
+       for(int i=1;i<n+1;i++){
+           //背包的最大容量
+           for (int j=1;j<m+1;j++){
+               //物品的重量小于背包的容量
+               for (int k=0;k*w[i-1]<= j;k++){
+                   //里面包含可以放置0到n个的所有情况
+                   dp[i][j]=Math.max(dp[i-1][j],dp[i-1][j-k*w[i-1]]+k*v[i-1]);
+               }
+
+           }
+       }
+     return dp[n][m];
+   }
+
+
+   /**
+    *
+    * 多重背包问题（加上物品的数量限制）
+    * f[i][v]=max{f[i-1][v-k*c[i]]+k*w[i]|0<=k*c[i]<=v} 物品可以加多次
+     * 完全背包问题(每个物品不限量)
+     * @param w
+     * @param v
+     * @return
+     */
+   public static int packageMany(int[] w,int[] v,int n,int m,int[] nums){
+       //表示前i件物品恰好放入重量为m的背包时的最大价值
+       int[][] dp=new int[n+1][m+1];
+
+       //初始化数组
+       for(int i=0;i<n+1;i++){
+           dp[i][0] = 0;
+       }
+       for(int j=0;j<m+1;j++){
+           dp[0][j] = 0;
+       }
+        //物品的编号
+       for(int i=1;i<n+1;i++){
+           //背包的最大容量
+           for (int j=1;j<m+1;j++){
+               //物品的重量小于背包的容量
+
+               //背包数量限制
+               for (int k=0; k<=nums[i-1];k++){
+                   //多个物品小于背包的数量，可以申请加入
+                   if (k*w[i-1] <= j){
+                       dp[i][j]=Math.max(dp[i-1][j],dp[i-1][j-k*w[i-1]]+k*v[i-1]);
+                   }
+               }
+
+           }
+       }
+     return dp[n][m];
+   }
 
     /**
      * 【题目】：
