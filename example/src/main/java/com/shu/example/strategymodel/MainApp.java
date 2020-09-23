8e75c52196fa2861014caf4951952398bfc64cc8
shu616048151 <616048151@qq.com>
2020-09-23 11:25:43 +0800

    策略模式设计方式

---
+++ example/src/main/java/com/shu/example/strategymodel/MainApp.java
@@ -0,0 +1,19 @@
+package com.shu.example.strategymodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * @author shuxibing
+ * @date 2019/8/4 14:28
+ * @uint d9lab
+ * @Description:
+ */
+//去除某项配置
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class MainApp {
+    public static void main(String[] args){
+        SpringApplication.run(MainApp.class,args);
+    }
+}
---
+++ example/src/main/java/com/shu/example/strategymodel/OrderService.java
@@ -0,0 +1,11 @@
+package com.shu.example.strategymodel;
+
+/**
+ * @author shuxibing
+ * @date 2019/7/31 14:36
+ * @uint d9lab
+ * @Description:
+ */
+public interface OrderService {
+    String handle(OrderDTO orderDTO);
+}
